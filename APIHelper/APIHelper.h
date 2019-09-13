2b855aa627c5f83f40bf1b6b67284297ed92f1c5
Amish Banjara <amish.banjara@gmail.com>
2019-09-13 16:38:27 -0500

    Initial Commit

---
+++ APIHelper/APIHelper.h
@@ -0,0 +1,19 @@
+//
+//  APIHelper.h
+//  APIHelper
+//
+//  Created by Amish on 14/09/19.
+//  Copyright © 2019 Amish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APIHelper.
+FOUNDATION_EXPORT double APIHelperVersionNumber;
+
+//! Project version string for APIHelper.
+FOUNDATION_EXPORT const unsigned char APIHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIHelper/PublicHeader.h>
+
+
---
+++ APIHelper/APIHelper.swift
@@ -0,0 +1,100 @@
+//
+//  APIHelper.swift
+//  APIHelper
+//
+//  Created by Amish on 14/09/19.
+//  Copyright © 2019 Amish. All rights reserved.
+//
+
+import UIKit
+
+public class APIHelper {
+    
+    public static let sharedInstance = APIHelper()
+    
+    internal var apiBus:[URLRequest] = [URLRequest]()
+    public typealias completionBlock = (Data?,URLResponse?,Error?) -> Void
+    internal var block: completionBlock?
+    internal var isDispatchingRequests: Bool = false
+    
+    
+    public func authenticateUser(withUrl url: URL, httpMethod: HttpMethod, parameters: Data?, header: [String:String]?,completionHandler:@escaping completionBlock) {
+        block = completionHandler
+    }
+    
+    public func callWebservice(withUrl url: URL, httpMethod: HttpMethod, parameters: Data?, header: [String:String]?,completionHandler:@escaping completionBlock) {
+        block = completionHandler
+    }
+    
+}
+
+extension APIHelper {
+    
+    public enum HttpMethod: String {
+        case get
+        case post
+        
+        func value() -> String {
+            switch self {
+            case .get:
+                return "GET"
+            case .post:
+                return "POST"
+            }
+        }
+    }
+    
+    private func callAPI(url: URL, httpMethod: HttpMethod, parameters: Data?, header: [String:String]?) {
+        if Reachability.isConnectedToNetwork() {
+            var request = URLRequest(url: url)
+            request.httpMethod = httpMethod.value()
+            request.httpBody = parameters
+            request.allHTTPHeaderFields = header
+            createAndExecuteURlSession(withRequest: request)
+        } else {
+            block!(nil,nil,nil)
+        }
+    }
+    
+    private func createAndExecuteURlSession(withRequest request: URLRequest) {
+        let session = URLSession.shared
+        DispatchQueue.global(qos: .default).async {
+            let dataTask: URLSessionDataTask = session.dataTask(with: request) { [weak self] (data, response, error) in
+                guard let httpResponse = response as? HTTPURLResponse, let strongSelf = self else {
+                    return
+                }
+                if httpResponse.statusCode == 401 {
+                    if strongSelf.apiBus.contains(request) {
+                        strongSelf.apiBus.append(request)
+                    }
+                    
+                    if !strongSelf.isDispatchingRequests {
+                        strongSelf.startDispatching()
+                    }
+                    
+                } else {
+                    strongSelf.block!(data,response,error)
+                }
+            }
+            dataTask.resume()
+        }
+    }
+    
+    private func startDispatching() {
+        if !apiBus.isEmpty {
+            for request in apiBus {
+                isDispatchingRequests = true
+                let operation = Operation()
+                let randomNum = Int.random(in: 0 ..< Int.max)
+                operation.name = randomNum.description
+                operation.qualityOfService = .background
+                operation.completionBlock = { [weak self] in
+                    self?.createAndExecuteURlSession(withRequest: request)
+                }
+                operation.start()
+            }
+            isDispatchingRequests = false
+        }
+    }
+
+}
